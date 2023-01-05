fe8f5a3db339beb838382054507d2b45b328fbde
Bauka <e444er@gmail.com>
2023-01-05 23:30:29 +0600

    Initial commit

---
+++ app/src/main/java/com/e444er/ai_dall_e/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.e444er.ai_dall_e
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
